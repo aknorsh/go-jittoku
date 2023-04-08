dc5a98cf5c13795db287bfad31602e6c3d106c1b
aknorsh <aknorsh@gmail.com>
2023-04-08 23:47:26 +0900

    add spew

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aknorsh/go-jittoku
 
 go 1.20
+
+require github.com/davecgh/go-spew v1.1.1
--- print.go
+++ print.go
@@ -1,6 +1,13 @@
 package jittoku
 
+import "github.com/davecgh/go-spew/spew"
+
 // PrintNyan 標準エラー出力に猫を表示する
 func PrintNyan() {
 	println("########## 😺 ##########")
+}
+
+// Dump spew.Dumpを呼ぶ。標準出力にダンプ。
+func Dump(a ...interface{}) {
+	spew.Dump(a...)
 }
